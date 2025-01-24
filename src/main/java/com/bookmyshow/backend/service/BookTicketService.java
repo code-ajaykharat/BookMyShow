166aa1a4db03bce8cfe1abfc78c264da7a7461fa
ajay kharat <iamajaykharat@gmail.com>
2025-01-25 00:48:59 +0530

    BookMyShow: Spring version updated

--- src/main/java/com/bookmyshow/backend/service/BookTicketService.java
+++ src/main/java/com/bookmyshow/backend/service/BookTicketService.java
@@ -1,9 +1,11 @@
 package com.bookmyshow.backend.service;
 
 import com.bookmyshow.backend.model.Booking;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface BookTicketService {
     Booking bookTicket(int userId, int showId, List<Integer> showSeatIds);
 }
